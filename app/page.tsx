5537bf02923f1dd2fc90392d0b9963d7350cd713
abuzar <abuzardawar444@gmail.com>
2024-12-08 23:15:53 +0500

    `Update dependencies and remove unnecessary whitespace`

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import LoadingContainer from '@/components/global/LoadingContainer';
-import FeaturedProducts from '@/components/home/FeaturedProducts';
-import Hero from '@/components/home/Hero';
+import LoadingContainer from "@/components/global/LoadingContainer";
+import FeaturedProducts from "@/components/home/FeaturedProducts";
+import Hero from "@/components/home/Hero";
 import React, { Suspense } from "react";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <Suspense fallback={<LoadingContainer />}>
-        <FeaturedProducts />
-      </Suspense>
+      <Suspense fallback={<LoadingContainer />}></Suspense>
     </>
   );
 };
